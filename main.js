04cb94bf118b23b707331d0810fd7a3c24352811
someshg-del <76991902+someshg-del@users.noreply.github.com>
2021-07-11 21:44:32 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+function bed()
+{
+  window.location="bed.html";
+}
+function ac()
+{
+  window.location="ac.html";
+}
+function tv()
+{
+  window.location="tv.html";
+}
+function cool()
+{
+  window.location="fridge.html";
+}
+function table()
+{
+  window.location="table.html";
+}
